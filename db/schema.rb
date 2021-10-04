feeab7e0956ee732eef62035f9f9477553413cbc
jonrichmond <jrichmond124@gmail.com>
2021-10-04 13:46:17 -0400

    enhancement: Switch to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,15 @@
 
 ActiveRecord::Schema.define(version: 2021_10_04_121947) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "hunters", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_hunters_on_user_id"
   end
 
