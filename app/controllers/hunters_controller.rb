1df8b75ecf385155e2bdfe8dbec43c6fa46634da
jonrichmond <jrichmond124@gmail.com>
2021-10-04 07:45:52 -0400

    enhancement: CRUD a Hunter

---
+++ app/controllers/hunters_controller.rb
@@ -0,0 +1,49 @@
+class HuntersController < ApplicationController
+  def index
+    @hunters = Hunter.all
+  end
+
+  def show
+    @hunter = Hunter.find params[:id]
+  end
+
+  def new
+    @hunter = Hunter.new
+  end
+
+  def create
+    @hunter = Hunter.new hunter_params
+    if @hunter.save
+      redirect_to @hunter
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @hunter = Hunter.find params[:id]
+  end
+
+  def update
+    @hunter = Hunter.find params[:id]
+
+    if @hunter.update(hunter_params)
+      redirect_to @hunter
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @hunter = Hunter.find(params[:id])
+    @hunter.destroy
+
+    redirect_to root_path
+  end
+
+  private
+
+  def hunter_params
+    params.require(:hunter).permit(:name, :description)
+  end
+end
